db56aafb4e105e5c9bce9d69156c03ed5ac61a10
jumaschion <julia.maschion@hotmail.com>
2018-12-04 17:11:31 -0200

    Instalacao de jquery, validate.js

---
+++ script/validacao.js
@@ -0,0 +1,24 @@
+let form = $(".formulario-contato-home");
+
+form.validate({
+    errorElement: "li",
+    rules: {
+      email: {
+        required: true,
+        email: true
+      },
+      name: "required",
+      subject: "required",
+      message: "required"
+    },
+    messages: {
+      email: {
+        required: "We need your email address to contact you / Precisamos do seu e-mail para entrar em contato.",
+        email: "Your email address must be in the format of name@domain.com / Seu e-mail deve ser no formato nome@dominio.com."
+      },
+        name: "Please specify your name / Por favor, digite seu nome.",
+        subject: "Please write a subject / Por favor, escreva o assunto.",
+        message: "Please, write a message / Por favor, escreva uma mensagem."
+      }
+
+  });
