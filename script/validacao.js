21dfb79992ac78e0f40207bf1edf441ec7ccbda3
jumaschion <julia.maschion@hotmail.com>
2018-12-05 15:37:46 -0200

    Configurando forms

--- script/validacao.js
+++ script/validacao.js
@@ -17,9 +17,9 @@ form.validate({
         email: "Seu e-mail deve ser no formato nome@dominio.com."
       },
         name: "Por favor, digite seu nome.",
-        subject: "Por favor, escreva o assunto.",
-        message: "Por favor, escreva uma mensagem."
       }
 
   });
+
+
 
