0c23ce505cc9fb2e5547a3a187f8671cda8a8ebf
jumaschion <julia.maschion@hotmail.com>
2018-12-05 22:44:44 -0200

    Formulario de telefone com validação

--- script/validacao.js
+++ script/validacao.js
@@ -51,6 +51,10 @@ formCamisas.validate({
 
   });
 
+ function isInputNumber(evt) {
+    let ch = String.fromCharCode(evt.which);
 
+    if (!(/[0-9]/.test(ch))){
+      evt.preventDefault()
 
-
+} }
