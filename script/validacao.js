f3228c32cad2cf9b312162ce6c4f1f24cc793032
jumaschion <julia.maschion@hotmail.com>
2018-12-06 12:11:11 -0200

    Validação de formulários feita

--- script/validacao.js
+++ script/validacao.js
@@ -24,16 +24,30 @@ form.validate({
   let formCamisas = $(".form-camisas");
 
 formCamisas.validate({
-    errorElement: "li",
+    errorElement: "div",
+    errorPlacement: function(error, element) {
+      let type = $(element).attr("type");
+      if(type === 'radio' ) {
+        error.insertAfter(element.closest('.form-item'))
+      } else {
+        error.insertAfter(element);
+      }
+    },
     rules: {
       email: {
         required: true,
         email: true
       },
+      camisaGenero: "required",
+      camisa: "required",
+      colarinho: "required",
+      punho: "required",
+      vista: "required",
+      bolso: "required",
+      tamanho: "required",
       name: "required",
       tel: "required",
-      tecido: "required",
-      
+      tecido: "required"
       
     },
     messages: {
@@ -41,14 +55,20 @@ formCamisas.validate({
         required: "Precisamos do seu e-mail para entrar em contato.",
         email: "Seu e-mail deve ser no formato nome@dominio.com."
       },
-        name: "Por favor, digite seu nome.",
-        tel: "Digite o telefone",
-        message: "Por favor, escreva uma mensagem.",
-        tecido: "Escolha um tecido",
-        
-  
-      }
+      name: "Por favor, digite seu nome.",
+      tel: "Digite o telefone.",
+      message: "Por favor, escreva uma mensagem.",
+      genero: "Por favor, escolhe o gênero.",
+      tecido: "Escolha um tecido.",
+      camisaGenero: "Escolha o estilo da camisa.",
+      camisa: "Escolha o estilo da camisa.",
+      colarinho: "Escolha o estilo do colarinho.",
+      punho: "Escolha o estilo do punho.",
+      vista: "Escolha o estilo da vista.",
+      bolso: "Escolha se a sua camisa vai ter bolso.",
+      tamanho: "Escolha o tamanho da sua casmisa."
 
+      }
   });
 
  function isInputNumber(evt) {
